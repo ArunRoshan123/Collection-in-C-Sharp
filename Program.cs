38c5ec54929a3a9a0f59b23549d718175c20f3a2
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-03 22:27:13 +0530

    [Arun] Add: Implementing Queue in C sharp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,26 +11,20 @@ namespace Collection
     {
         static void Main(string[] args)
         {
-            
-            List<int> l1 = new List<int>();
-            l1.Add(1);
-            l1.Add(2);
-            l1.Add('a');
-            l1.RemoveAt(2);
-            l1.Reverse();
-            l1.Sort();
-            l1[0] = 4;
-            for(int i=0;i< l1.Count;i++)
+
+            Queue q1 = new Queue();
+
+            q1.Enqueue(1);
+            q1.Enqueue(2);
+            q1.Enqueue("Arun");
+            q1.Enqueue('A');
+
+            Console.WriteLine("Peek");
+            Console.WriteLine(q1.Peek());
+            q1.Dequeue();
+            foreach (object l1 in q1)
             {
-                Console.WriteLine(l1[i]);
-            }
-            Console.WriteLine(l1.Count());
-            Console.WriteLine(l1.Contains(2));
-            Console.WriteLine(l1.Capacity);
-            l1.Clear();
-            for(int i=0;i< l1.Count;i++)
-            {
-                Console.WriteLine(l1[i]);
+                Console.WriteLine(l1);
             }
         }
 
