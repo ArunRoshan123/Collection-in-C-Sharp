5790849351f7796532f25bd85b8b13952e445938
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-04 12:29:12 +0530

    [Arun] Add: Implementing Stack in C sharp

--- Program.cs
+++ Program.cs
@@ -11,20 +11,41 @@ namespace Collection
     {
         static void Main(string[] args)
         {
+            Stack s1 = new Stack();
+            Stack <int> s2= new Stack<int>();
 
-            Queue q1 = new Queue();
+            // using non-genric method
+            s1.Push(1);
+            s1.Push(2);
+            s1.Push(3);
+            s1.Push(4);
+            s1.Push(5);
+            
+            // using genric method
+            s2.Push(1);
+            s2.Push(2);
+            s2.Push(3);
+            s2.Push(4);
+            s2.Push(5);
 
-            q1.Enqueue(1);
-            q1.Enqueue(2);
-            q1.Enqueue("Arun");
-            q1.Enqueue('A');
-
-            Console.WriteLine("Peek");
-            Console.WriteLine(q1.Peek());
-            q1.Dequeue();
-            foreach (object l1 in q1)
+            s1.Push("Arun");
+            s1.Push("Roshan");
+            s1.Push("A R");
+            foreach(object o in s1)
             {
-                Console.WriteLine(l1);
+                Console.WriteLine(o);
+            }
+            s1.Pop();
+            Console.WriteLine("The top element is");
+            Console.WriteLine(s1.Peek());
+            Console.WriteLine("Number of elements: " + s1.Count );
+            s1.Clear();
+            
+            Console.WriteLine();
+            Console.WriteLine("Generic Method");
+            foreach(object o1 in s2)
+            {
+                Console.WriteLine(o1);
             }
         }
 
